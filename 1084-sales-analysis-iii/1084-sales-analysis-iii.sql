1b992ba861ff2d4c5ee1b773395e1bc77cf91c00
martinezreices <86532963+martinezreices@users.noreply.github.com>
2026-02-06 12:32:14 -0500

    {name} [Easy] - .sql | Time: 1279 ms (5.08%), Space: 0B (100%)

---
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -0,0 +1,10 @@
+
+-- Only products sold in Q1 2019 (2019-01-01 & 2019-03-31)
+
+SELECT p.product_id, p.product_name
+FROM Product p
+LEFT JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING
+    MIN(s.sale_date) >= '2019-01-01'
+    AND MAX(s.sale_date) <= '2019-03-31'; 
